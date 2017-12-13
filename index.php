0ae6aa99824e4e97a2c481049528426bad43fa2b
wuweiqiang1993 <wuweiqiang1993@github.com>
2017-12-14 00:09:40 +0800

    ~

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
     declare(strict_types=1); //开启强类型检查，必须写在文件开头
     // 应用目录为当前目录
     define('APP_PATH', __DIR__ . '/');
-
     // 开启调试模式
     define('APP_DEBUG', true);
-    
     require(APP_PATH . 'Lib/base.php');
-    
     // 实例化框架类
     Base::run();
